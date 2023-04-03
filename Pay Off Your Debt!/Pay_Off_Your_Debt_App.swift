65e303dced64d4a52eeab24913d43ccf85ef7e39
Ziterz <ziady.mubaraq@gmail.com>
2023-04-03 09:06:42 +0700

    Initial Commit

---
+++ Pay Off Your Debt!/Pay_Off_Your_Debt_App.swift
@@ -0,0 +1,17 @@
+//
+//  Pay_Off_Your_Debt_App.swift
+//  Pay Off Your Debt!
+//
+//  Created by Ziady Mubaraq on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Pay_Off_Your_Debt_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
